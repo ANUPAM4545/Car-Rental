700df1777e56973ff9368296923393e2a96c1c58
ANUPAM4545 <anupamsingh8095@gmail.com>
2026-04-02 15:24:40 +0530

    Configure app for Vercel deploy

--- server.js
+++ server.js
@@ -196,7 +196,9 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
